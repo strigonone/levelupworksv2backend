64966023260cca36ab8872b2acb787533d1c4571
strigonone <s15jaychao@hotmail.com>
2021-06-21 20:13:27 +1200

    Backend early stages Ready

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const MongoClient = require("mongodb").MongoClient;
 const mongoose = require("mongoose");
+const Student = require("./student");
+const express = require("express");
+
+// express app
+const app = express();
 
 // connect to mongodb
 const uri =
 	"mongodb+srv://gar1080:gar6969@missionreadyproject6.xoipl.mongodb.net/MissionReadyLevelUpWorks?retryWrites=true&w=majority";
 mongoose
 	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then((result) => console.log("Connected to DB"))
+	.then((result) => app.listen(8080))
 	.catch((err) => console.log(err));
 
 const client = new MongoClient(uri, {
@@ -18,3 +23,36 @@ client.connect((err) => {
 	// perform actions on the collection object
 	client.close();
 });
+
+// mongoose and mongo sandbox routes
+
+// Adding student manually from backend
+app.get("/api/add-student", (req, res) => {
+	const student = new Student({
+		name: "Ricardo",
+		email: "ricardo@email.com",
+	});
+
+	student
+		.save()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+// Getting all students or mongodb collections
+app.get("/api/all-students", (req, res) => {
+	Student.find()
+		.then((result) => {
+			res.send(result);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+const port = process.env.PORT || 6969;
+app.listen(port, () => console.log(`Server ready at port ${port}`));
---
+++ student.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+const Student = mongoose.model("Student", studentSchema);
+module.exports = Student;
